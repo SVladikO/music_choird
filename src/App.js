6a6f255bba1df0df8385d26b871e9bbe56d1428d
svladiko <vlad.serhiychuk@gmail.com>
2024-11-28 11:07:27 +0200

    Fixed ui issues.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import {BrowserRouter, Routes, Route} from "react-router";
 
 import ChordsPage from './page/chords/chords.page';
 import NotePage from './page/Notes/Note.page';
@@ -11,18 +11,18 @@ import {page_urls} from "./config";
 
 function App() {
     return (
-        <>
+        <div>
             <Header/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path={page_urls.home} element={<div>Home</div>} />
-                    <Route path={page_urls.chords} element={<ChordsPage/>} />
-                    <Route path={page_urls.notes} element={<NotePage/>} />
-                    <Route path={page_urls.about} element={<div>About</div>} />
-                </Routes>
-            </BrowserRouter>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={page_urls.home} element={<div>Home</div>}/>
+                        <Route path={page_urls.chords} element={<ChordsPage/>}/>
+                        <Route path={page_urls.notes} element={<NotePage/>}/>
+                        <Route path={page_urls.about} element={<div>About</div>}/>
+                    </Routes>
+                </BrowserRouter>
             <DonateInfo/>
-        </>
+        </div>
     )
 }
 
